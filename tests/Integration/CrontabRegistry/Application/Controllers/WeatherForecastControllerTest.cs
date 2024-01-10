4b85a276d1489dd48bb261baccb1f6431542a135
Rafał Salamon <rasa@salamonrafal.pl>
2024-01-10 18:25:00 +0100

    #1 Prepare structure of project:
    * Added Integration test for example endpoint

--- tests/Integration/CrontabRegistry/Application/Controllers/WeatherForecastControllerTest.cs
+++ tests/Integration/CrontabRegistry/Application/Controllers/WeatherForecastControllerTest.cs
@@ -1,11 +1,62 @@
-﻿namespace Integration.CrontabRegistry.Application.Controllers
+﻿using CrontabRegistry.Domain.Models;
+using FluentAssertions;
+using Integration.Helpers;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Integration.CrontabRegistry.Application.Controllers
 {
     public class WeatherForecastControllerTest
     {
-        [Test]
-        public void GetWeatherForecast_should_return_random_data()
-        {
+        private WebApplicationFactory<Program> _clientApiFactory;
+        private HttpClient _httpClient;
 
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _clientApiFactory = new CustomWebApplicationFactory<Program>();
+            _httpClient = _clientApiFactory.CreateClient();
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _clientApiFactory.Dispose();
+            _httpClient.Dispose();
+        }
+
+        [Test]
+        public async Task GetWeatherForecast_should_return_random_data()
+        {
+            // arrange
+            var expectedSummaries = new[]
+            {
+                "Freezing",
+                "Bracing",
+                "Chilly",
+                "Cool",
+                "Mild",
+                "Warm",
+                "Balmy",
+                "Hot",
+                "Sweltering",
+                "Scorching"
+            };
+
+            // act
+            var response = await _httpClient.GetAsync("WeatherForecast");
+            var contentType = response.Content.Headers.ContentType!.ToString();
+            var responseJsonContent = HtmlContentHelper.GetJsonContent<WeatherForecastModel[]>(
+                await response.Content.ReadAsStringAsync()
+                );
+
+            // assert
+            response.EnsureSuccessStatusCode();
+            contentType.Should().Contain("application/json; charset=utf-8");
+            responseJsonContent.Should().NotBeNullOrEmpty();
+            responseJsonContent.Should().AllSatisfy(x =>
+            {
+                x.Summary.Should().ContainAny(expectedSummaries);
+            });
         }
     }
 }
